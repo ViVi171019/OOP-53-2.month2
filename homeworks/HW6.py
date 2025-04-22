7ce530f468b34c88a11bbb599981877e62ab1d6e
ViVi171019 <wselavii@gmail.com>
2025-04-22 20:26:42 +0600

    HW6

---
+++ homeworks/HW6.py
@@ -0,0 +1,13 @@
+def two_sum(nums, target):
+    num_to_index = {}
+
+    for index, num in enumerate(nums):
+        complement = target - num
+        if complement in num_to_index:
+            return [num_to_index[complement], index]
+        num_to_index[num] = index
+
+nums = [3, 7, 8, 11]
+target = 15
+result = two_sum(nums, target)
+print(result)
